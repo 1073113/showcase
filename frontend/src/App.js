0283dd177afd880f604e8d634f219587a133be98
David Ho <>
2021-06-25 22:35:10 +0800

    setup showcase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/showcase/HomeScreen'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import ProductScreen from './screens/showcase/ProductScreen'
 
 const App = () => {
   return (
-   <>
-   <h1>Welcome to Dav Shop</h1>
-   </>
-  );
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/" component={HomeScreen} exact />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
